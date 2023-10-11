e15ebec2d0cf69490d7a1f04a06d5db0b662a5b5
nguyent0275 <nguyent0275@gmail.com>
2023-10-10 22:43:37 -0400

    formatted code and revised saveScore function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
-// timer location in html 
+// timer location in html
 var timerEl = document.getElementById("countdown");
 // start button location in html
 var startButton = document.getElementById("start-button");
-// suntmit button location in html
+// submit button location in html
 var submitButton = document.getElementById("submit-button");
+// time left in the timer
 var timeLeft = 20;
 // index for which question is being displayed
 var state = 0;
 // the value of the user's number of correct answers divided by the total amount of questions
 var score = 0;
+submitButton.style.visibility = "hidden";
 
 // timer function start
 function countdown() {
@@ -18,11 +20,12 @@ function countdown() {
     // displaying the timeLeft with, "Time:" prefacing it in a textarea in the html's main
     timerEl.textContent = "Time : " + timeLeft;
     // if the timer hits 0 seconds, the timer will dissapear and calculate score
-    if (timeLeft == 0) {
-      let score = state / (myQuestions.length) * 100
+    if (timeLeft <= 0) {
+      let score = (state / myQuestions.length) * 100;
       // displaying the score as an alert
-      alert("You scored a " + score)
-      saveScore()
+      // document.body.innerHTML = "";
+      alert("You scored a " + score);
+      saveScore();
       // cancels the timer
       clearInterval(timeInterval);
     }
@@ -31,6 +34,12 @@ function countdown() {
 }
 // timer function end
 
+// variables that store the text for the quiz introductions
+var quizIntro = document.getElementById("quiz");
+var quizP = document.getElementById("quiz-p");
+quizIntro.textContent = "Coding Quiz Challenge";
+quizP.textContent =
+  "Try to answer the following javascript-related questions within the time limit. Keep in mind that incorrect answers will penalize your time by 10 seconds";
 
 // start button function
 function buttonStart() {
@@ -39,6 +48,11 @@ function buttonStart() {
   // runs countdown and loadquestion functions
   countdown();
   loadQuestion();
+  // shows submit button on click
+  submitButton.style.visibility = "visible";
+  // removes the text from the quiz introductions
+  quizIntro.remove();
+  quizP.remove();
 }
 // start button function end
 
@@ -68,7 +82,7 @@ var myQuestions = [
   {
     question: "3. How do you create a function?",
     answers: [
-      { ans: "function:myFunction()", isCorrect: false},
+      { ans: "function:myFunction()", isCorrect: false },
       { ans: "function=myFunction()", isCorrect: false },
       { ans: "function myFunction()", isCorrect: true },
       { ans: "myFunction():function", isCorrect: false },
@@ -77,8 +91,8 @@ var myQuestions = [
   {
     question: "4. How do you call a function named 'myFunction'?",
     answers: [
-      { ans: "call myFunction()", isCorrect: false},
-      { ans: "myFunction()", isCorrect: true},
+      { ans: "call myFunction()", isCorrect: false },
+      { ans: "myFunction()", isCorrect: true },
       { ans: "call function myFunction", isCorrect: false },
       { ans: "Call.myFunction()", isCorrect: false },
     ],
@@ -86,9 +100,9 @@ var myQuestions = [
   {
     question: "5. How does a 'for' loop start?",
     answers: [
-      { ans: "for (i = 0; i <= 5)", isCorrect: false},
-      { ans: "for (i = 0; i <= 5; i++)", isCorrect: true},
-      { ans: "for i = 1 to 5", isCorrect: false},
+      { ans: "for (i = 0; i <= 5)", isCorrect: false },
+      { ans: "for (i = 0; i <= 5; i++)", isCorrect: true },
+      { ans: "for i = 1 to 5", isCorrect: false },
       { ans: "for (i <= 5; i++)", isCorrect: false },
     ],
   },
@@ -99,11 +113,11 @@ function loadQuestion() {
   // where the questions and choices are being displayed in html
   var questions = document.getElementById("questions");
   var choices = document.getElementById("choices");
-  // how the questions are being displayed in html 
+  // how the questions are being displayed in html
   questions.textContent = myQuestions[state].question;
-  // setting the text of choices in html to blank / 
+  // setting the text of choices in html to blank /
   choices.textContent = "";
-  // looping through the objects in the answers array 
+  // looping through the objects in the answers array
   for (let i = 0; i < myQuestions[state].answers.length; i++) {
     // creating a div, input, and label within html
     const choicesdiv = document.createElement("div");
@@ -131,7 +145,7 @@ function loadQuestion() {
     answers += myQuestions[state].answers[i].ans;
     // sets the choices to a type of radio inpt
     choice.type = "radio";
-    // 
+    //
     choice.name = "answer";
     choice.value = i;
     // displaying the answers in html
@@ -153,36 +167,38 @@ function submitAnswer() {
   let checkedValue = document.querySelector(
     'input[name="answer"]:checked'
   ).value;
-  // sets a variable to easily access the boolean key value pairs associated with the choices 
+  // sets a variable to easily access the boolean key value pairs associated with the choices
   var isCorrect = myQuestions[state].answers[checkedValue].isCorrect;
   // checking if user input's choice has the corresponding key value pair of isCorrect
   if (isCorrect) {
     // if they do, then state increases and next question is loaded
     state++;
     // if the state of the question increases past the length(amount) of the myQuestions array
-  if (state !== (myQuestions.length))
-    loadQuestion();
-  // if they answer wrong, the timeLeft is decreased by 10seconds 
+    if (state !== myQuestions.length) loadQuestion();
+    // if they answer wrong, the timeLeft is decreased by 10seconds
   } else timeLeft -= 10;
   timerEl.textContent = "Timer : " + timeLeft;
 
   // if the state reaches the end of the array
-  if (state == (myQuestions.length)){
+  if (state == myQuestions.length) {
     // score will be calculated based on the current state divided by total questions. (multplied by 100 to display as percentage)
-    let score = state / (myQuestions.length) * 100
+    let score = (state / myQuestions.length) * 100;
     // displays the score as an alert box
-    alert("You scored a " + score)
+    // document.body.innerHTML = "";
+    alert("You scored a " + score);
+    saveScore()
   }
-  // save score function will run 
-  saveScore()
+  // save score function will run
+  // saveScore()
 }
 
 // save score function start
-function saveScore () {
-  let savedName = document.createElement("input")
-  let savedScore = document.createElement("h1")
+function saveScore() {
+  var savedInitial = document.createElement("h2")
+  var savedScore = document.createElement("h3")
+  body.appendChild(savedInitial)
+  body.appendChild(savedScore)
 
-  savedScore = "Congratulations your score is " + score
-  savedName = "Enter your name here: "
-  
-  }
+  savedInitial.textContent = "Please save your initials here: "
+  savedScore.textContent = "Your new high score is: " + score
+}
